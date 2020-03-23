e814384ee835b2d968980c1081b0f45c3aebe17f
Jeremy CARRUBA <Michelmoustache@MacBook-Pro-2.local>
2020-03-23 11:41:55 +0100

    Création du squelette

--- boutique/routes/web.php
+++ boutique/routes/web.php
@@ -17,6 +17,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
+Route::get('/catalog', function () {
+    return view('catalog');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
