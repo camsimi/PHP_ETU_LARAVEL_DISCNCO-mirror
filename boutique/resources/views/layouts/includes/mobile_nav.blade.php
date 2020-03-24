570826b7e71a7da94b363f9726376ec6bf021fff
Jeremy CARRUBA <Michelmoustache@MacBook-Pro-2.local>
2020-03-24 17:04:28 +0100

    Navbar mobile

--- boutique/resources/views/layouts/includes/mobile_nav.blade.php
+++ boutique/resources/views/layouts/includes/mobile_nav.blade.php
@@ -1,1 +1,11 @@
-<?php
+<nav class="mobile_nav">
+    <div>
+        <a class="home"><img src="{{ asset('media/icons/logo.svg') }}" alt="home button"></a>
+    </div>
+    <div>
+        <li><a class="searchbar"><img src="{{ asset('media/icons/search_icon.svg') }}" alt="searchbar button"></a></li>
+        <li><a class="burger"><img src="{{ asset('media/icons/burger.svg') }}" alt="menu button"></a></li>
+        <li><a class="cart"><img src="{{ asset('media/icons/cart.svg') }}" alt="cart button"></a></li>
+        <li><a class="member"><img src="{{ asset('media/icons/member.svg') }}" alt="member button"></a></li>
+    </div>
+</nav>
