96ceb5852795b27b9588d1e7669d9384ab6c346c
Jeremy CARRUBA <Michelmoustache@MacBook-Pro-2.local>
2020-03-24 13:42:58 +0100

    Branche master - Routes créées

--- boutique/resources/views/home.blade.php
+++ boutique/resources/views/home.blade.php
@@ -1,11 +1,10 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Home - Discn'co</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -82,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Homepage
+                    Home
                 </div>
 
                 <div class="links">
@@ -97,9 +96,5 @@
                 </div>
             </div>
         </div>
-
-
-
     </body>
 </html>
-
