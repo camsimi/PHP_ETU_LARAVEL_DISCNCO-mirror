7acadc6a24a32c34ee8e76089bf9184ba5e4aa07
Jeremy CARRUBA <Michelmoustache@MacBook-Pro-2.local>
2020-03-25 15:37:23 +0100

    Vues home-mobile pre-bootstraps

--- boutique/resources/views/home.blade.php
+++ boutique/resources/views/home.blade.php
@@ -6,24 +6,65 @@
 @endsection
 
 @section('content')
-<div class="releases">
-    <h2>New Releases</h2>
+
+<div class="releases category">
+    <h2>NEW RELEASES</h2>
+    <div class ="carrousel">
+        <span><img class="arrow" src="{{ asset('media/icons/left_arrow.svg')}}" alt="left arrow"></span>
+        <span><img class ="cover" src="{{ asset('media/covers/vacuum.jpg')}}" alt="album cover"></span>
+        <span><img class="arrow" src="{{ asset('media/icons/right_arrow.svg')}}" alt="right arrow"></span>
+    </div>
+    <div class="buttons">
+        <span><img class="play" src="{{ asset('media/buttons/play_black.svg')}}" alt="play button"></span>
+        <span><img class="cart" src="{{ asset('media/buttons/cart_button.svg')}}" alt="add into cart button"></span>
+    </div>
 </div>
 
-<div class="pergenre">
-    <h2>Genres</h2>
+<div class="pergenre category">
+    <div class="carrousel">
+        <span><img class="arrow" src="{{ asset('media/icons/left_arrow.svg')}}" alt="left arrow"></span>
+        <h2>GENRES</h2>
+        <span><img class="arrow" src="{{ asset('media/icons/right_arrow.svg')}}" alt="right arrow"></span>
+    </div>
+
+    <div class="week_selection">
+        <div class="week_album">
+            <section><img class ="cover" src="{{ asset('media/covers/kindofblue.jpg')}}" alt="Album of the week cover"></section>
+            <h3>Album of the week</h3>
+        </div>
+
+        <div class="week_single">
+            <section><img class ="cover" src="{{ asset('media/covers/lovesupreme.jpg')}}" alt="Single of the week cover"></section>
+            <h3>Single of the week</h3>
+        </div>
+    </div>
+    <img src="{{ asset('media/buttons/see_more.svg')}}">
 </div>
 
-<div class="turntables">
-    <h2>Turntables</h2>
+<div class="turntables category">
+    <h2>TURNTABLES</h2>
+    <div class="product_box">
+        <img src="{{ asset('media/images/turntable.png')}}" alt="turntable photo">
+        <img class="button" src="{{ asset('media/buttons/see_more.svg')}}">
+    </div>
 </div>
 
-<div class="accessories">
-    <h2>Accessories</h2>
+<div class="accessories category">
+    <h2>ACCESSORIES</h2>
+    <div class="product_box">
+        <img src="{{ asset('media/images/accessories.jpg')}}" alt="accessories photo">
+        <img class="button" src="{{ asset('media/buttons/see_more.svg')}}">
+    </div>
 </div>
 
-<div class="newsletter">
-    <h2>Newsletter</h2>
+<div class="newsletter category">
+    <h2>NEWSLETTER</h2>
+    <form action="post">
+        <div class="row">
+            <input class="col textbox" type="text" placeholder="sign in for the newsletter ...">
+            <button type="col submit"><img src="{{ asset('media/buttons/mail.svg')}}" alt="mail button"></button>
+        </div>
+    </form>
 </div>
 
 @endsection
---
+++ boutique/resources/views/layouts/includes/footer.blade.php
@@ -0,0 +1,12 @@
+    <div class="row">
+        <div class="col follow">
+            <div>
+                <img class="social" src="{{ asset('/media/icons/facebook.svg')}}" alt="">
+                <img class="social" src="{{ asset('/media/icons/instagram.svg')}}" alt="">
+                <img class="social" src="{{ asset('/media/icons/twitter.svg')}}" alt="">
+            </div>
+        </div>
+        <div class="col contact">
+            <p>Contact us</p>
+        </div>
+    </div>
