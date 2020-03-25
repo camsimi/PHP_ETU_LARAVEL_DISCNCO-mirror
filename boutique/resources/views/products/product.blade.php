e981814456b82cec51741faee3e005eb43a3f63a
bryan <bryan.steg@le-campus-numerique.fr>
2020-03-25 16:29:52 +0100

    ca marche ?

--- boutique/resources/views/products/product.blade.php
+++ boutique/resources/views/products/product.blade.php
@@ -1,4 +1,24 @@
 @extends('../layouts.app')
 @section('css')
 <link href="{{ asset('css/style_products.css') }}" rel="stylesheet">
+
+@endsection
+@section('content')
+<div>
+    <div>
+    <img src="" alt="">
+    </div>
+    <div>
+        <div>
+            <h3>Titre de la musique</h3>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+</div>
 @endsection
