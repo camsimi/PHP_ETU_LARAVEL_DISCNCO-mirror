3d80a15e6c8fbd4053ad7d235a56349f0172b877
bryan <bryan.steg@le-campus-numerique.fr>
2020-03-25 17:01:53 +0100

    ont avance enfin

--- boutique/resources/views/products/product.blade.php
+++ boutique/resources/views/products/product.blade.php
@@ -1,24 +1,31 @@
 @extends('../layouts.app')
 @section('css')
-<link href="{{ asset('css/style_products.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style_products.css') }}" rel="stylesheet">
 
 @endsection
 @section('content')
-<div>
-    <div>
-    <img src="" alt="">
-    </div>
     <div>
         <div>
-            <h3>Titre de la musique</h3>
-
+            <img src="{{ asset('media/images/turntable.png')}}" alt="turntable">
         </div>
         <div>
+            <div class="container flex-wrap">
+                <h3>Titre de la musique</h3>
+                <p>genre</p>
+                <p>Date</p>
+                <p>classement</p>
 
-        </div>
-        <div>
-
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto earum, facilis itaque
+                    labore magnam, magni nostrum provident quae qui vero voluptas. Architecto blanditiis esse in, ipsa
+                    maxime qui recusandae?</p>
+            </div>
+            <div>
+                <form>
+                    <input type="button">
+                </form>
+            </div>
         </div>
     </div>
-</div>
 @endsection
