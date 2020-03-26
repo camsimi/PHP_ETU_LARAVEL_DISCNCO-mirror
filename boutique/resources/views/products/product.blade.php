316d2cd4e48d80029f34975fa3944033c187d7e5
bryan <bryan.steg@le-campus-numerique.fr>
2020-03-26 11:16:20 +0100

    squelette page produit

--- boutique/resources/views/products/product.blade.php
+++ boutique/resources/views/products/product.blade.php
@@ -4,16 +4,16 @@
 
 @endsection
 @section('content')
-    <div>
+    <div class="container d-lg-inline-flex justify-content-lg-around">
         <div>
             <img src="{{ asset('media/images/turntable.png')}}" alt="turntable">
         </div>
         <div>
-            <div class="container flex-wrap">
-                <h3>Titre de la musique</h3>
-                <p>genre</p>
-                <p>Date</p>
-                <p>classement</p>
+            <h3>Titre de la musique</h3>
+            <div class="container flex-row d-flex justify-content-around">
+                <p class="p-2">genre</p>
+                <p class="p-2">Date</p>
+                <p class="p-2">classement</p>
 
             </div>
             <div>
@@ -21,8 +21,10 @@
                     labore magnam, magni nostrum provident quae qui vero voluptas. Architecto blanditiis esse in, ipsa
                     maxime qui recusandae?</p>
             </div>
-            <div>
+            <div class="container d-flex flex-row justify-content-around">
+                <p>Prix unitaire : 75 € </p>
                 <form>
+                    <input type="number" value="0">
                     <input type="button">
                 </form>
             </div>
