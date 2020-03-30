f2b406842512d80306168f6f3714ff0f58025336
Camille <simiand.camille@gmail.com>
2020-03-30 15:00:35 +0200

    RAW SQL QUERIES avec la bdd ilot ok sauf chemin image

---
+++ boutique/app/Http/Controllers/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+
+class ProductController extends Controller {
+
+//    Afficher tous les produits du catalogue
+    public function index() {
+        $products = DB::select('select * from products');
+        return view ('products.catalog', ['products' => $products]);
+    }
+
+//    Afficher un produit
+//    public function show($id) {
+//        $product = DB::select('select * from products WHERE id = :id', ['id' => $id]);
+//        return view('products.product', ['product' => $product]);
+//    }
+}
